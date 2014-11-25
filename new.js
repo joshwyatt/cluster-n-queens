be603e590ab8383a685880afda762733b3752d9b
joshWyatt <joshuawyatt123@gmail.com>
2014-11-25 10:40:52 -0800

    Write skeleton and notes on what needs doing

--- new.js
+++ new.js
@@ -1,3 +1,19 @@
 //LET'S DO THIS RIGHT
 
-//i need to be ab
+//i need to be able to caluclate the solutions for a given n, passing in a board state.
+//if the board state will consist of already attacked squares and which row we are starting at.
+//in this way anyone with this algo can be entirely agnostic of what needs doing and we can just
+  //pass in args and get what we need.
+
+
+function boardStateGenerator(n, rowToStartAt){
+  var boardState;
+
+  return boardState;
+}
+
+function calculateSolutions(n, rowToStartAt){
+  var solutions = 0;
+
+  return solutions;
+}
