6e397bf94be1db8aa09be4c76fccb00073072ac2
joshWyatt <joshuawyatt123@gmail.com>
2014-11-26 15:08:27 -0800

    Run worker for every cpu as long as there are function arguments to call.

--- master.js
+++ master.js
@@ -3,33 +3,37 @@
   //core modules
   var cluster = require('cluster');
 
+  var total = 0;
+  //just a cheezy way to get a bunch of fn arguments
+  var fnArgs = [];
+  for(var j = 0; j < 100; j++){
+    fnArgs.push(j);
+  }
+  
   function master(){
-    var total = 0;
-
-    //just a cheezy way to get a bunch of fn arguments
-    var fnArgs = [];
-    for(var j = 0; j < 100; j++){
-      fnArgs.push(j);
-    }
-
-    console.log('master module is beginning');
-
     var numberOfCPUs = require('os').cpus().length;
     for(var i = 0; i < numberOfCPUs; i++){
-      var args = fnArgs[i];
-      var worker = cluster.fork({
-        args: args
-      });
-
-      worker.on('message', function(msg){
-        total += +msg;
-        console.log('inside master and here is the new total after msg - ' + msg + ': ' + total);
-      });
-
-      worker.on('exit', function(code, signal){
-        console.log('in exit. code: ' + code + ' signal: ' + signal);
-      });
+      var args = fnArgs.pop();
+      runWorker(args);
     }
+  }
+
+  function runWorker(args){
+    var worker = cluster.fork({
+      args: args
+    });
+
+    worker.on('message', function(msg){
+      total += +msg;
+    });
+
+    worker.on('exit', function(code, signal){
+      if( fnArgs.length > 0 ){
+        runWorker(fnArgs.pop());
+      }else{
+        console.log('total: ' + total);
+      }
+    });
   }
 
   module.exports.master = master;
--- worker.js
+++ worker.js
@@ -3,14 +3,9 @@
   var cluster = require('cluster');
 
   function worker(){
-    console.log('worker is starting');
     var args = process.env.args;
     process.send(args);
     cluster.worker.kill();
-  }
-
-  function tester(a, b){
-    return 'a is "' + a + '" and b is "' + b + '"';
   }
 
   module.exports.worker = worker;
