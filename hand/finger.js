39a499003583cf6af700ff928fe20e3b2310db06
joshWyatt <joshuawyatt123@gmail.com>
2014-11-26 16:48:35 -0800

    Begin connecting finger server to cabeza server

--- hand/finger.js
+++ hand/finger.js
@@ -1,14 +1,18 @@
 //core modules
 var http = require('http');
 var os = require('os');
-var request = require('request');
-
 //command line args
 var cabezaLocation = process.argv[2];
 
 var numberOfCPUs = os.cpus().length;
 
+http.get(cabezaLocation, function(res){
+  res.on('data', function(data){
+    console.log('inside finger here is the response from cabeza: ' + data);
+  });
+});
+
 //calculate number of cpus
 //send number of cpus as a request to cabezaAddress
 //on response from cabeza, run that shit on all the cpus
-  //return result to cabeza
+  //return result to cabez
